ca32a3bbf7ca867952335f29fe576f8213273f5f
xxqLgnd <yvangengeryt@mail.ru>
2025-07-13 03:26:38 +0300

    Update

--- HWID.lua
+++ HWID.lua
@@ -1,8 +1,7 @@
 local HWIDTable = {
     "d25d6598-4112-4086-98db-605d0774e31b",
     "f38bd558-b910-4425-86eb-ba04e5de5827",
-    "15b2cf8b-139c-4e71-ac46-ba710cf2c148",
-    "d8fb1ac1-3e3c-4c46-889f-514ec80ef16d"
+    "15b2cf8b-139c-4e71-ac46-ba710cf2c148"
 }
 
 return HWIDTable
