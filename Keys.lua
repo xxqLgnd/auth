79315bbffe1d37688148928946fa54d1d41e13dd
Artem <artemygodly666@mail.ru>
2025-07-22 11:59:25 +0300

    Initial clean commit)

---
+++ Keys.lua
@@ -0,0 +1,15 @@
+local Library = {
+    ["xxqLgnd_a7K2q9"] = "0df5f41a-8e30-4de3-84fd-2de481ee26e5",
+    ["xxqLgnd_X5b8L2"] = "df777bfa-a480-4f38-adce-0c3e37bf9983",
+    ["xxqLgnd_pQ4m8z"] = "15B2cf8b-139c-4e71-ac46-ba710cf2c148",
+    ["xxqLgnd_9Jh2l7"] = "7d233b8b-6742-4ff8-b813-3c49f67551d8",
+    ["xxqLgnd_w3N7x5"] = "a80b1ffd-7832-4a32-87d1-b6d30db17d2a",
+    ["xxqLgnd_2z8Qk1"] = "",
+    ["xxqLgnd_M6t4v9"] = "",
+    ["xxqLgnd_lQ7b2X"] = "",
+    ["xxqLgnd_8v5C1r"] = "",
+    ["xxqLgnd_k3N9w6"] = "",
+    ["xxqLgnd_Z7x2m4"] = "",
+}
+
+return Library
